aa406cf6af67a3038d79aeb2e3cf29bb5c3d2dd0
gfoster1 <georgepfoster@gmail.com>
2015-03-03 09:47:21 -0600

    updating express version and formatting to heroku standards

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
+app.set('port', (process.env.PORT || 5000));
 app.engine('jade', require('jade').__express);
 
 app.get("/", function(req, res){
@@ -25,13 +26,7 @@ app.get("/interesting", function(req, res){
 });
 app.use("/public", express.static(__dirname + '/public'));
 app.use("/images", express.static(__dirname + '/images'));
- 
-var port = 3700;
-var io = require('socket.io').listen(app.listen(port));
-io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
-});
-console.log("Listening on port " + port);
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+}); 
